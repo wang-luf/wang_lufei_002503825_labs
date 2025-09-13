2eeb846ae7dd001b71f9e4088a6c952119e41bed
wang-luf <wang.luf@northeastern.edu>
2025-09-13 15:32:24 -0700

    Add Address and Product

--- Lab 1/src/Model/Product.java
+++ Lab 1/src/Model/Product.java
@@ -9,5 +9,63 @@ package Model;
  * @author l
  */
 public class Product {
+    String name;
+    String description;
+    String availNum;
+    String price;
+    Address manufactureAddress;
+    Address shippingAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Address getManufactureAddress() {
+        return manufactureAddress;
+    }
+
+    public void setManufactureAddress(Address manufactureAddress) {
+        this.manufactureAddress = manufactureAddress;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+    
+    
+    
     
 }
+    
